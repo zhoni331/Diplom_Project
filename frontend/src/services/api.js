3ce0cd1806f35955b0a27f2ba14c0169297ae465
zhoni331 <zanibektljsibekov341@gmail.com>
2026-05-13 17:27:04 +0600

    переход на урл бэк сервиса

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+
+const API_URL = import.meta.env.VITE_API_URL
+
 const api = axios.create({
-    baseURL:    'http://127.0.0.1:8000',
+    //baseURL:    'http://127.0.0.1:8000',
+    baseURL: API_URL,
 });
 
 api.interceptors.request.use((config) => {
